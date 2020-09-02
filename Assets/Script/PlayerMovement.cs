f9001e1ffac9f50ff48a67d87779d3edaa7178e0
Andy SHU <andy091045@gmail.com>
2020-09-02 14:41:05 +0800

    local first

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     //玩家AB
     public PlayerType playerType;
-    public float speed = 5;
+    //public float speed = 5;
     public event Action<Vector2, Vector2> eShoot; //射出(目前位置,方向)
     Rigidbody2D rigid;
     Vector2 velocity = new Vector2();
@@ -17,19 +17,46 @@ public class PlayerMovement : MonoBehaviour
     [Range(0.01f, 1)]
     public float ringSize = 0.2f;
 
+    public float walkGapTime = 0.2f;
+    public float moveGapDistance = 1;
+    Coroutine cWalkGap;
+
+    Animator animator;
+    SpriteRenderer sp;
+
     private void Start()
     {
         rigid = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+        sp = gameObject.GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
-        //velocity = rigid.velocity;
 
         //WASD按鍵移動
         if (playerType == PlayerType.A)
         {
-            velocity = new Vector2(Input.GetAxis("AHorizontal"), Input.GetAxis("AVertical")) * speed;
+            //velocity = new Vector2(Mathf.CeilToInt(Input.GetAxis("AHorizontal")), Mathf.CeilToInt(Input.GetAxis("AVertical"))) * speed;
+            if (cWalkGap == null)
+            {
+                if (Input.GetKey(KeyCode.A))
+                {
+                    cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(-1, 0) * moveGapDistance));
+                    sp.flipX = false;
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(1, 0) * moveGapDistance));
+                    sp.flipX = true;
+                }
+                else if (Input.GetKey(KeyCode.W))
+                {
+                    cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(0, 1) * moveGapDistance));
+                }
+                else if (Input.GetKey(KeyCode.S)) { cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(0, -1) * moveGapDistance)); }
+            }
+
             //續力
             if (Input.GetKey(KeyCode.Space))
             {
@@ -50,13 +77,30 @@ public class PlayerMovement : MonoBehaviour
                 {
                     eShoot(ball.transform.position, dir.normalized);
                 }
+                animator.Play("Attack");
             }
         }
 
         //上下左右移動
         if (playerType == PlayerType.B)
         {
-            velocity = new Vector2(Input.GetAxis("BHorizontal"), Input.GetAxis("BVertical")) * speed;
+            //velocity = new Vector2(Input.GetAxis("BHorizontal"), Input.GetAxis("BVertical")) * speed;
+            if (cWalkGap == null)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(-1, 0) * moveGapDistance));
+                    sp.flipX = false;
+                }
+                else if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(1, 0) * moveGapDistance));
+                    sp.flipX = true;
+                }
+                else if (Input.GetKey(KeyCode.UpArrow)) { cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(0, 1) * moveGapDistance)); }
+                else if (Input.GetKey(KeyCode.DownArrow)) { cWalkGap = StartCoroutine(WalkTo_Coro(new Vector2(0, -1) * moveGapDistance)); }
+
+            }
             //續力
             if (Input.GetKey(KeyCode.L))
             {
@@ -75,17 +119,35 @@ public class PlayerMovement : MonoBehaviour
                     Vector2 dir = ball.transform.position - gameObject.transform.position;
                     eShoot(ball.transform.position, dir.normalized);
                 }
+                animator.Play("Attack");
             }
         }
         rigid.velocity = velocity;
 
 
     }
-
+    private void FixedUpdate()
+    {
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsTag("move"))
+        {
+            animator.Play("Idle");
+        }
+    }
 
     public enum PlayerType
     {
         A, B
+    }
+
+
+    IEnumerator WalkTo_Coro(Vector2 dir)
+    {
+        Vector2 goal_pos = (Vector2)transform.position + dir;
+        animator.Play("Dash");
+        transform.position = goal_pos;
+        yield return new WaitForSeconds(walkGapTime);
+        cWalkGap = null;
+
     }
 
 }
